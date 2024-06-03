49ba252968570348f380ef7713c1299e22650b5e
ppppyzhkoff <pavel071204@gmail.com>
2024-06-04 00:07:01 +0200

    Enable communication with Google Sheets.

--- lab_4/code/index.php
+++ lab_4/code/index.php
@@ -45,6 +45,38 @@
         <th>Title</th>
         <th>Description</th>
         </thead>
+        <tbody>
+        <?php
+        require 'vendor/autoload.php';
+        error_reporting(E_ALL & ~E_DEPRECATED & ~E_NOTICE);
+
+        $client = new Google_Client();
+        $client->setApplicationName('php-lab-4');
+        $client->setScopes(['https://www.googleapis.com/auth/spreadsheets']);
+        $client->setAccessType('offline');
+        $path = 'credentials.json';
+        $client->setAuthConfig($path);
+
+        $service = new Google_Service_Sheets($client);
+
+        $spreadsheetId = '1hWwXz2-Hsro7PHD-r5ifk3wLdAgEzWI1N68BHWALuhI';
+        $spreadsheet = $service->spreadsheets->get($spreadsheetId);
+        $sheets = $spreadsheet->getSheets();
+        foreach ($sheets as $sheet) {
+            $sheetTitle = $sheet->getProperties()->getTitle();
+
+            $response = $service->spreadsheets_values->get($spreadsheetId, $sheetTitle);
+            $values = $response->getValues();
+            if (!empty($values) && count($values) >= 4) {
+                $category = htmlspecialchars($values[0][0]);
+                $title = htmlspecialchars($values[1][0]);
+                $email = htmlspecialchars($values[2][0]);
+                $description = htmlspecialchars($values[3][0]);
+
+            }
+        }
+        ?>
+        </tbody>
     </table>
 </div>
 </body>
--- lab_4/code/save.php
+++ lab_4/code/save.php
@@ -2,6 +2,17 @@
 require 'vendor/autoload.php';
 error_reporting(E_ALL & ~E_DEPRECATED & ~E_NOTICE);
 
+
+$client = new Google_Client();
+$client->setApplicationName('php-lab-4');
+$client->setScopes(['https://www.googleapis.com/auth/spreadsheets']);
+$client->setAccessType('offline');
+$client->setAuthConfig('credentials.json');
+
+$service = new Google_Service_Sheets($client);
+
+$spreadsheetId = '1hWwXz2-Hsro7PHD-r5ifk3wLdAgEzWI1N68BHWALuhI';
+
 if ('POST' === $_SERVER['REQUEST_METHOD']) {
     $category = $_POST['categories'] ?? '';
     $title = $_POST['title'] ?? '';
@@ -12,4 +23,16 @@ if ('POST' === $_SERVER['REQUEST_METHOD']) {
     ];
 }
 
+$range = 'List1';
+
+$body = new Google_Service_Sheets_ValueRange([
+    'values' => $data
+]);
+$params = [
+    'valueInputOption' => 'RAW'
+];
+
+$result = $service->spreadsheets_values->append($spreadsheetId, $range, $body, $params);
+
+header('Location: index.php');
 exit();
